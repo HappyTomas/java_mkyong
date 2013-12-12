9b94bbd225acf0a6087670dca12b02f962a356da
Filip Turkiewicz <filip.turkiewicz+github@gmail.com>
2013-12-12 21:43:16 +0100

    spring mvc

---
+++ src/main/java/com/mkyong/common/controller/MovieController.java
@@ -0,0 +1,40 @@
+package com.mkyong.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * MovieController
+ */
+@Controller
+@RequestMapping("/movie")
+public class MovieController {
+
+	/**
+	 * @param name
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public String getMovie(@PathVariable String name, ModelMap model) {
+
+		model.addAttribute("movie", name);
+
+		return "list";
+	}
+
+	/**
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getDefaultMovie(ModelMap model) {
+
+		model.addAttribute("movie", "this is default movie");
+
+		return "list";
+	}
+}
